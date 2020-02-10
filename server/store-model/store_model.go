6ebb2e46ee9d6266ce944b7bd9b9fba5f6b2d284
Oleg Borodin <onborodin@gmail.com>
2020-02-10 22:11:59 +0200

    add store_model

---
+++ server/store-model/store_model.go
@@ -0,0 +1,146 @@
+/*
+ * Copyright 2019 Oleg Borodin  <borodin@unix7.org>
+ */
+
+
+package storeModel
+
+import (
+    //"math/rand"
+    "log"
+    //"strings"
+    //"errors"
+
+    "github.com/jmoiron/sqlx"
+    //_ "github.com/jackc/pgx/v4/stdlib"
+    //_ "github.com/mattn/go-sqlite3"
+
+
+)
+
+const schema = `
+    DROP TABLE IF EXISTS stores;
+    CREATE TABLE IF NOT EXISTS stores (
+        id          INTEGER PRIMARY KEY,
+        type        VARCHAR(255) NOT NULL,
+        schema      VARCHAR(255) NOT NULL,
+        hostname    VARCHAR(255) NOT NULL UNIQUE,
+        port        INTEGER NOT NULL,
+        username    VARCHAR(255) NOT NULL,
+        password    VARCHAR(255) NOT NULL
+    );`
+
+type Model struct {
+    db *sqlx.DB
+}
+
+type Agent struct {
+    Id          int     `db:"id"        json:"id"`
+
+    Type        string  `db:"type"      json:"type"`
+    Schema      string  `db:"schema"    json:"schema"`
+    Hostname    string  `db:"hostname"  json:"hostname"`
+    Port        int     `db:"port"      json:"port"`
+
+    Username    string  `db:"username" json:"username"`
+    Password    string  `db:"password" json:"password"`
+}
+
+type Page struct {
+    Total           int         `json:"total"`
+    Offset          int         `json:"offset"`
+    Limit           int         `json:"limit"`
+    AgentPattern    string      `json:"store_pattern"`
+    Agents          *[]Agent    `json:"stores,omitempty"`
+}
+
+func (this *Model) Migrate() error {
+    _, err := this.db.Exec(schema)
+    if err != nil {
+        log.Println(err)
+        return err
+    }
+    return nil
+}
+
+func (this *Model) List(page *Page) (error) {
+    var request string
+    var err error
+    var total int
+
+    storePattern := "%" + page.AgentPattern + "%"
+    request = `SELECT COUNT(id) as total FROM stores WHERE hostname LIKE $1`
+    err = this.db.QueryRow(request, storePattern).Scan(&total)
+    if err != nil {
+        log.Println(err)
+        return err
+    }
+    page.Total = total
+
+    var stores []Agent
+    request = `SELECT id, type, schema, hostname, port, username, '' as password
+                FROM stores
+                WHERE hostname LIKE $1
+                ORDER BY hostname
+                LIMIT $2 OFFSET $3`
+
+    err = this.db.Select(&stores, request, storePattern, page.Limit, page.Offset)
+    if err != nil {
+        log.Println(err)
+        return err
+    }
+    page.Agents = &stores
+    return nil
+}
+
+func (this *Model) Create(store Agent) error {
+    request := `INSERT INTO stores(type, schema, hostname, port, username, password)
+                VALUES ($1, $2, $3, $4, $5, $6)`
+    _, err := this.db.Exec(request, store.Type, store.Schema, store.Hostname, store.Port, store.Username, store.Password)
+    if err != nil {
+        log.Println(err)
+        return err
+    }
+    return nil
+}
+
+func (this *Model) Delete(store Agent) error {
+    request := `DELETE FROM stores WHERE id = $1`
+    _, err := this.db.Exec(request, store.Id)
+    if err != nil {
+        log.Println(err)
+        return err
+
+    }
+    return nil
+}
+
+func (this *Model) Update(store Agent) error {
+    var err error
+    if len(store.Password) > 0 {
+        request := `UPDATE stores
+                    SET type = $1, schema = $2, hostname = $3, port = $4, username = $5, password = $6
+                    WHERE id = $7`
+        _, err = this.db.Exec(request, store.Type, store.Schema, store.Hostname,
+                                        store.Port, store.Username, store.Password, store.Id)
+    } else {
+        request := `UPDATE stores
+                    SET type = $1, schema = $2, hostname = $3, port = $4, username = $5
+                    WHERE id = $6`
+        _, err = this.db.Exec(request, store.Type, store.Schema,store.Hostname,
+                                        store.Port, store.Username, store.Id)
+    }
+    if err != nil {
+        log.Println(err)
+        return err
+    }
+    return nil
+}
+
+
+func New(db *sqlx.DB) *Model {
+    model := Model{
+        db: db,
+    }
+    return &model
+}
---
+++ server/store-model/store_model_test.go
@@ -0,0 +1,129 @@
+/*
+ * Copyright 2020 Oleg Borodin  <borodin@unix7.org>
+ */
+
+package storeModel
+
+import (
+    "testing"
+
+    //"github.com/stretchr/testify/assert"
+
+    "github.com/jmoiron/sqlx"
+    _ "github.com/mattn/go-sqlite3"
+
+    "os"
+    "fmt"
+)
+
+func createDB() (*sqlx.DB, error) {
+    db, err := sqlx.Open("sqlite3", "test.db")
+    if err != nil {
+        return db, err
+    }
+    err = db.Ping()
+    if err != nil {
+        return db, err
+    }
+    return db, nil
+}
+
+func TestCreate(t *testing.T) {
+
+    var err error
+    os.Remove("test.db")
+
+    dbx, err := createDB()
+    if err != nil {
+        t.Error(err)
+    }
+
+    model := New(dbx)
+    store := Agent{
+        Schema:     "https",
+        Hostname:   "localhost",
+        Port:       7002,
+        Username:   "user1",
+        Password:   "12345",
+    }
+
+    err = model.Migrate()
+    if err != nil {
+        t.Error(err)
+    }
+
+    err = model.Create(store)
+    if err != nil {
+        t.Error(err)
+    }
+
+    var stores []Agent
+    page := Page{
+        Limit: 500,
+        Agents: &stores,
+    }
+    err = model.List(&page)
+    if err != nil {
+        t.Error(err)
+    }
+    fmt.Println(*page.Agents)
+
+}
+
+func TestUpdate(t *testing.T) {
+
+    dbx, err := createDB()
+    if err != nil {
+        t.Error(err)
+    }
+
+    model := New(dbx)
+    store := Agent{
+        Schema:     "http",
+        Hostname:   "some.bare",
+        Port:       7007,
+        Username:   "user21",
+        Password:   "qwerty",
+    }
+
+    err = model.Migrate()
+    if err != nil {
+        t.Error(err)
+    }
+
+    err = model.Create(store)
+    if err != nil {
+        t.Error(err)
+    }
+
+    var stores []Agent
+    page := Page{
+        Limit: 500,
+        Agents: &stores,
+    }
+    err = model.List(&page)
+    if err != nil {
+        t.Error(err)
+    }
+    fmt.Println(*page.Agents)
+}
+
+
+func TestDelete(t *testing.T) {
+
+    dbx, err := createDB()
+    if err != nil {
+        t.Error(err)
+    }
+
+    model := New(dbx)
+    store := Agent{
+        Id: 1,
+    }
+
+    err = model.Delete(store)
+    if err != nil {
+        t.Error(err)
+    }
+
+}
