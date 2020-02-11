678f9728ab2e3204a7c07c330027becee8d4a7aa
Oleg Borodin <onborodin@gmail.com>
2020-02-11 15:35:56 +0200

    rebuild form; clean from legacy code

--- front/src/app/users/users.component.ts
+++ front/src/app/users/users.component.ts
@@ -20,27 +20,18 @@ interface PaginatorItem {
 })
 export class UsersComponent implements OnInit {
 
-    createForm: FormGroup
-    updateForm: FormGroup
-    deleteForm: FormGroup
-
     searchForm: FormGroup
 
     noticeMessage: string = null
     alertMessage: string = null
 
-    users: User[]
-
     page: Page = {
         total: 0,
-        limit: 3,
+        limit: 5,
         offset: 0,
         user_pattern: "",
-        users: null
+        users: []
     }
-
-    minUsernameLength: number = 5
-    minPasswordLength: number = 5
 
     constructor(
         private formBuilder: FormBuilder,
@@ -57,153 +48,24 @@ export class UsersComponent implements OnInit {
             (response: ResponsePage) => {
                 if (response.error == false) {
                     this.page = response.result
-                    this.paginate()
+                } else {
+                    if (response.message != null) {
+                        this.alertMessage = "Backend error: " + response.message
+                    } else {
+                        this.alertMessage = "Backend error."
+                    }
                 }
             },
             (error) => {
-                console.log(error)
-                this.users = null
+                if (error.message != null) {
+                    this.alertMessage = "Communication error: " + error.message
+                } else {
+                    this.alertMessage = "Communication error."
+                }
             }
         )
     }
 
-    createUser(event) {
-        var payload: User = {
-                username: event.value.username,
-                password: event.value.password
-        }
-        this.dismissCreateForm()
-        this.userService.create(payload).subscribe(
-            (response: ResponseUser) => {
-                if (response.error == true) {
-                    this.showAlert("Operation error")
-                    return
-                }
-                this.showNotice("User created")
-                this.list()
-            },
-            (error) => {
-                this.showAlert("Connection error")
-                console.log(error)
-            }
-        )
-    }
-
-    updateUser(event) {
-        var payload: User = {
-                id: event.value.id,
-                username: event.value.username,
-                password: event.value.password
-        }
-        this.dismissUpdateForm()
-
-        this.userService.update(payload).subscribe(
-            (response: ResponseUser) => {
-                if (response.error == true) {
-                    this.showAlert("Operation error")
-                    return
-                }
-                this.showNotice("User updated")
-                this.list()
-            },
-            (error) => {
-                this.showAlert("Connection error")
-                console.log(error)
-            }
-        )
-    }
-
-    deleteUser(event) {
-        console.log(event)
-        var payload: User = {
-                id: event.value.id,
-                username: event.value.username,
-                password: event.value.password
-        }
-        console.log(payload)
-        this.dismissDeleteForm()
-
-        this.userService.deletex(payload).subscribe(
-            (response: ResponseUser) => {
-                if (response.error == true) {
-                    this.showAlert("Operation error")
-                    return
-                }
-                this.showNotice("User deleted")
-                this.list()
-            },
-            (error) => {
-                this.showAlert("Connection error")
-                console.log(error)
-            }
-        )
-    }
-
-
-    /* Create form */
-    showCreateForm() {
-        $("#create-user").modal('show')
-    }
-
-    dismissCreateForm() {
-        $("#create-user").modal('hide')
-        this.createForm.reset()
-    }
-
-    get createFormUsername(){
-       return this.createForm.get('username')
-    }
-    get createFormPassword(){
-       return this.createForm.get('password')
-    }
-
-    /* Update form */
-    showUpdateForm(item: User) {
-        console.log(item)
-        this.updateForm.reset()
-        this.updateForm.patchValue({
-            id: item.id,
-            username: item.username,
-            password: null
-        })
-        $("#update-user").modal('show')
-    }
-
-    dismissUpdateForm() {
-        $("#update-user").modal('hide')
-        this.updateForm.reset()
-    }
-
-    get updateFormUsername(){
-       return this.updateForm.get('username')
-    }
-    get updateFormPassword(){
-       return this.updateForm.get('password')
-    }
-
-    /* Delete form */
-    showDeleteForm(item: User) {
-        this.deleteForm.patchValue({
-            id: item.id,
-            username: item.username,
-            password: null,
-            confirmation: false
-        })
-        $("#delete-user").modal('show')
-    }
-
-    get deleteFormUsername(){
-       return this.deleteForm.get('username')
-    }
-
-    get deleteFormAcceptTerm(){
-       return this.deleteForm.get('acceptTerm')
-    }
-
-    dismissDeleteForm() {
-        $("#delete-user").modal('hide')
-        this.deleteForm.reset()
-    }
 
     /* Alerts */
     showAlert(message: string) {
@@ -221,105 +83,17 @@ export class UsersComponent implements OnInit {
         this.noticeMessage = null
     }
 
-    /* Pagination */
-    pageCount(total: number, limit: number): number {
-        //console.log(total, limit)
-        var pagesRaw: number= total/limit
-        var pagesLow: number = Math.floor(pagesRaw)
-        var pages: number = 0
-        if (pagesRaw == pagesLow) {
-            pages = pagesLow
-        } else {
-            pages = pagesLow + 1
-        }
-        return pages
-    }
-
-    resolvePageNumber(offset: number, limit: number, maxPage: number): number {
-        var p: number = 0
-        for (p = 0; p < maxPage; p++) {
-            if (offset >= p * limit && offset < (p + 1) * limit) {
-                return p
-            }
-        }
-        return 0
-    }
-
-    currentPage: number = 0
-    paginatorItems: PaginatorItem[]
-
-    changePage(offset: number) {
-        this.page.offset = offset
-        this.list()
-    }
-
-    paginate() {
-        var paginatorItems: PaginatorItem[] = []
-        var pageCount = this.pageCount(this.page.total, this.page.limit)
-        if (pageCount == 1) {
-            this.page.offset = 0
-        }
-        this.currentPage = this.resolvePageNumber(this.page.offset, this.page.limit, pageCount)
-        var i: number = 0
-
-        for (i = 0; i < pageCount; i++) {
-            if (i > this.currentPage - 4 && i < this.currentPage) {
-                var item: PaginatorItem = {
-                    visibleNumber: i + 1,
-                    offset: i * this.page.limit,
-                    active: false
-                }
-                paginatorItems.push(item)
-            }
-            if (i == this.currentPage) {
-                var item: PaginatorItem = {
-                    visibleNumber: i + 1,
-                    offset: i * this.page.limit,
-                    active: true
-                }
-                paginatorItems.push(item)
-            }
-            if (i > this.currentPage && i < this.currentPage + 4) {
-                var item: PaginatorItem = {
-                    visibleNumber: i + 1,
-                    offset: i * this.page.limit,
-                    active: false
-                }
-                paginatorItems.push(item)
-            }
-        }
-        this.paginatorItems = paginatorItems
-    }
-
     ngOnInit() {
-        this.createForm = this.formBuilder.group({
-            id: null,
-            username: [ "", [ Validators.required, Validators.minLength(this.minUsernameLength)] ],
-            password: [ "", [ Validators.required, Validators.minLength(this.minPasswordLength)] ]
-        })
-        this.updateForm = this.formBuilder.group({
-            id: null,
-            username: [ "", [ Validators.required, Validators.minLength(this.minUsernameLength)] ],
-            password: [ "", [ Validators.required, Validators.minLength(this.minPasswordLength)] ]
-        })
-        this.deleteForm = this.formBuilder.group({
-            id: null,
-            username: [ "" ],
-            password: [ "" ],
-            confirmation: [ false, Validators.requiredTrue ]
-
-        })
         this.searchForm = this.formBuilder.group({
             userPattern: [ "" ],
-            pageLimit: [ "" ],
+            pageLimit: [ this.page.limit ],
         })
 
         this.list()
-
 
         this.searchForm.get('userPattern').valueChanges
             .pipe(
-                debounceTime(400),
+                debounceTime(300),
                 distinctUntilChanged()
             )
             .subscribe(value => {
@@ -329,7 +103,7 @@ export class UsersComponent implements OnInit {
 
         this.searchForm.get('pageLimit').valueChanges
             .pipe(
-                debounceTime(400),
+                debounceTime(300),
                 distinctUntilChanged()
             )
             .subscribe(value => {
@@ -337,7 +111,6 @@ export class UsersComponent implements OnInit {
                 this.page.limit = value
                 this.list()
             })
-
     }
 
 }
