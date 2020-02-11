87c00e2883defd669e86b536795db7c3a41f9ec6
Oleg Borodin <onborodin@gmail.com>
2020-02-11 21:32:45 +0200

    add schedule-model

---
+++ server/schedule-model/schedule_model.go
@@ -0,0 +1,183 @@
+/*
+ * Copyright 2019 Oleg Borodin  <borodin@unix7.org>
+ */
+
+
+package agentModel
+
+import (
+    "log"
+    "github.com/jmoiron/sqlx"
+)
+
+const schema = `
+    DROP TABLE IF EXISTS schedules;
+    CREATE TABLE IF NOT EXISTS schedules (
+        id          INTEGER PRIMARY KEY,
+        agent_id    INTEGER NOT NULL,
+        store_id    INTEGER NOT NULL,
+        type        VARCHAR(255) NOT NULL,
+        store_path  VARCHAR(255) NOT NULL,
+        resourse    VARCHAR(255) NOT NULL,
+        mins        VARCHAR(255) NOT NULL,
+        hours       VARCHAR(255) NOT NULL,
+        wdays       VARCHAR(255) NOT NULL,
+        mdays       VARCHAR(255) NOT NULL,
+        depth       INTEGER NOT NULL
+    );`
+
+type Model struct {
+    db *sqlx.DB
+}
+
+type Schedule struct {
+    Id          int     `db:"id"        json:"id"`
+    AgentId     int     `db:"agent_id"  json:"agentId"`
+    StoreId     int     `db:"store_id"  json:"storeId"`
+    Type        string  `db:"type"      json:"type"` // dump, restore, ?copy
+    StorePath   string  `db:"store_path"  json:"id"`
+    Resourse    string  `db:"resourse"  json:"resourse"`
+    Mins        string  `db:"mins"      json:"mins"`
+    Hours       string  `db:"hours"     json:"hours"`
+    Mdays       string  `db:"mdays"     json:"mdays"`
+    Wdays       string  `db:"wdays"     json:"wdays"`
+    Depth       int     `db:"depth"     json:"depth"`
+}
+
+type Page struct {
+    Total           int         `json:"total"`
+    Offset          int         `json:"offset"`
+    Limit           int         `json:"limit"`
+    ResoursePattern string      `json:"resoursePattern"`
+    Schedules       *[]Schedule `json:"schedules"`
+}
+
+func (this *Model) Migrate() error {
+    _, err := this.db.Exec(schema)
+    if err != nil {
+        log.Println(err)
+        return err
+    }
+    return nil
+}
+
+func (this *Model) List(page *Page) (error) {
+    var request string
+    var err error
+    var total int
+
+    resoursePattern := "%" + page.ResoursePattern + "%"
+    request = `SELECT COUNT(id) as total FROM schedules WHERE resourse LIKE $1`
+    err = this.db.QueryRow(request, resoursePattern).Scan(&total)
+    if err != nil {
+        log.Println(err)
+        return err
+    }
+    page.Total = total
+
+    var schedules []Schedule
+    request = `SELECT
+                    agent_id,
+                    store_id,
+                    type,
+                    store_path,
+                    resourse,
+                    mins,
+                    hours,
+                    wdays,
+                    mdays,
+                    depth
+                FROM schedules
+                WHERE resourse LIKE $1
+                ORDER BY resourse
+                LIMIT $2 OFFSET $3`
+    err = this.db.Select(&schedules, request, resoursePattern, page.Limit, page.Offset)
+    if err != nil {
+        log.Println(err)
+        return err
+    }
+    page.Schedules = &schedules
+    return nil
+}
+
+func (this *Model) Create(schedule Schedule) error {
+    request := `INSERT INTO schedules(
+                    agent_id,
+                    store_id,
+                    type,
+                    store_path,
+                    resourse,
+                    mins,
+                    hours,
+                    wdays,
+                    mdays,
+                    depth)
+                VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10)`
+    _, err := this.db.Exec(request,
+            schedule.AgentId,
+            schedule.StoreId,
+            schedule.Type,
+            schedule.StorePath,
+            schedule.Resourse,
+            schedule.Mins,
+            schedule.Hours,
+            schedule.Wdays,
+            schedule.Mdays,
+            schedule.Depth)
+    if err != nil {
+        log.Println(err)
+        return err
+    }
+    return nil
+}
+
+
+func (this *Model) Update(schedule Schedule) error {
+    var err error
+    request := `UPDATE schedules
+                SET agent_id = $1,
+                    store_id = $2,
+                    type = $3,
+                    store_path = $4,
+                    resourse = $5,
+                    mins = $6,
+                    hours = $7,
+                    wdays = $8,
+                    mdays = $9,
+                    depth = $10
+                WHERE id = $11`
+
+    _, err = this.db.Exec(request,
+                    schedule.AgentId,
+                    schedule.StoreId,
+                    schedule.Type,
+                    schedule.StorePath,
+                    schedule.Resourse,
+                    schedule.Mins,
+                    schedule.Hours,
+                    schedule.Wdays,
+                    schedule.Mdays,
+                    schedule.Depth)
+    if err != nil {
+        log.Println(err)
+        return err
+    }
+    return nil
+}
+
+func (this *Model) Delete(agent Schedule) error {
+    request := `DELETE FROM schedules WHERE id = $1`
+    _, err := this.db.Exec(request, agent.Id)
+    if err != nil {
+        log.Println(err)
+        return err
+    }
+    return nil
+}
+
+func New(db *sqlx.DB) *Model {
+    model := Model{
+        db: db,
+    }
+    return &model
+}
---
+++ server/schedule-model/schedule_model_test.go
@@ -0,0 +1,138 @@
+/*
+ * Copyright 2020 Oleg Borodin  <borodin@unix7.org>
+ */
+
+package agentModel
+
+import (
+    "testing"
+    "os"
+    "fmt"
+
+    "github.com/jmoiron/sqlx"
+    _ "github.com/mattn/go-sqlite3"
+)
+
+func createDB() (*sqlx.DB, error) {
+    db, err := sqlx.Open("sqlite3", "test.db")
+    if err != nil {
+        return db, err
+    }
+    err = db.Ping()
+    if err != nil {
+        return db, err
+    }
+    return db, nil
+}
+
+func TestCreate(t *testing.T) {
+
+    var err error
+    os.Remove("test.db")
+
+    dbx, err := createDB()
+    if err != nil {
+        t.Error(err)
+    }
+
+    model := New(dbx)
+    schedule := Schedule{
+            AgentId: 1,
+            StoreId: 1,
+            Type: "dump",
+            StorePath: "/" ,
+            Resourse: "qwerty",
+            Mins: "5",
+            Hours: "5",
+            Wdays: "1",
+            Mdays: "2",
+            Depth: 10,
+    }
+
+    err = model.Migrate()
+    if err != nil {
+        t.Error(err)
+    }
+
+    err = model.Create(schedule)
+    if err != nil {
+        t.Error(err)
+    }
+
+    var schedules []Schedule
+    page := Page{
+        Limit: 500,
+        Schedules: &schedules,
+    }
+    err = model.List(&page)
+    if err != nil {
+        t.Error(err)
+    }
+    fmt.Println(*page.Schedules)
+
+}
+
+func TestUpdate(t *testing.T) {
+
+    dbx, err := createDB()
+    if err != nil {
+        t.Error(err)
+    }
+
+    model := New(dbx)
+    schedule := Schedule{
+            Id: 1,
+            AgentId: 2,
+            StoreId: 2,
+            Type: "restore",
+            StorePath: "/" ,
+            Resourse: "qwerty",
+            Mins: "5",
+            Hours: "5",
+            Wdays: "1",
+            Mdays: "2",
+            Depth: 10,
+    }
+
+    err = model.Migrate()
+    if err != nil {
+        t.Error(err)
+    }
+
+    err = model.Create(schedule)
+    if err != nil {
+        t.Error(err)
+    }
+
+    var schedules []Schedule
+    page := Page{
+        Limit: 500,
+        Schedules: &schedules,
+    }
+    err = model.List(&page)
+    if err != nil {
+        t.Error(err)
+    }
+    fmt.Println(*page.Schedules)
+}
+
+
+func TestDelete(t *testing.T) {
+
+    dbx, err := createDB()
+    if err != nil {
+        t.Error(err)
+    }
+
+    model := New(dbx)
+    schedule := Schedule{
+        Id: 1,
+    }
+
+    err = model.Delete(schedule)
+    if err != nil {
+        t.Error(err)
+    }
+
+    os.Remove("test.db")
+}
